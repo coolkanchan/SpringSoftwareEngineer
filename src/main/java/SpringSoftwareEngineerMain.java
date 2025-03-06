a1db0441cf7766ee342272d11e180bbce18c7122
coolkanchan <coolkanchan.shaw@gmail.com>
2025-03-06 03:09:29 -0600

    updated into maven project

---
+++ src/main/java/SpringSoftwareEngineerMain.java
@@ -0,0 +1,11 @@
+
+public class SpringSoftwareEngineerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello Software Engineering project");
+
+	}
+
+}
