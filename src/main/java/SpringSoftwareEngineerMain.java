1e89d454f0fa82192608a6591e361917b673550b
coolkanchan <coolkanchan.shaw@gmail.com>
2025-03-06 03:38:54 -0600

    updated text

--- src/main/java/SpringSoftwareEngineerMain.java
+++ src/main/java/SpringSoftwareEngineerMain.java
@@ -4,7 +4,7 @@ public class SpringSoftwareEngineerMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello Software Engineering project");
+		System.out.println("hello kanchan this is  Software Engineering project");
 
 	}
 
